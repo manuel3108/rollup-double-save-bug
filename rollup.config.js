392cdbc37b40c6c0d38a0f04ee94db20ad7f5920
Manuel Serret <mserret99@gmail.com>
2024-02-27 18:29:56 +0100

    remove useless stuff

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,12 @@
-import typescript from "@rollup/plugin-typescript";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import json from "@rollup/plugin-json";
-import dynamicImportVars from "@rollup/plugin-dynamic-import-vars";
-import fs from "fs";
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
+import fs from 'fs';
 
-// const composerFolders = fs
-//     .readdirSync("./projects/composers/", { withFileTypes: true })
-//     .filter((item) => item.isDirectory())
-//     .map((item) => item.name);
-// const composerNamesAsString = composerFolders.map((x) => `"${x}"`);
-
-function getConfig(project, isComposer) {
-    const input = `./projects/${project}/index.ts`
+function getConfig(project) {
+    const input = `./projects/${project}/index.ts`;
     const outDir = `./projects/${project}/build`;
     const tsConfig = `./projects/${project}/tsconfig.json`;
 
@@ -22,10 +16,14 @@ function getConfig(project, isComposer) {
         input: input,
         output: {
             dir: outDir,
-            format: "esm",
+            format: 'esm',
         },
         plugins: [
-            typescript({ tsconfig: tsConfig, module: "node16", include: ["**/*.ts", "**/package.json"] }),
+            typescript({
+                tsconfig: tsConfig,
+                module: 'node16',
+                include: ['**/*.ts', '**/package.json'],
+            }),
             nodeResolve({ preferBuiltins: true }),
             commonjs(),
             json(),
@@ -36,8 +34,4 @@ function getConfig(project, isComposer) {
     return config;
 }
 
-
-export default [
-    getConfig("core", false),
-    getConfig("tools", false),
-];
+export default [getConfig('core'), getConfig('tools')];
